8aec606783ae7e6d29c11ca1b341bace75dd86f2
André König <andre.koenig@openformation.io>
2022-10-06 16:42:26 +0000

    chore: adds base repo structure

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+/**
+ * @openformation/logto-remix
+ *
+ * Copyright, 2022 - Open Formation GmbH, Hamburg, Germany
+ *
+ * All rights reserved
+ */
+
+/**
+ * @author André König <andre.koenig@openformation.io>
+ *
+ */
